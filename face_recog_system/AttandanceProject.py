ca916d169e8f911a58f1f513989112cb50741512
manoramsubedi <manoramsubedi7@gmail.com>
2023-02-05 22:52:00 +0545

    attendance_sys

--- face_recog_system/AttandanceProject.py
+++ face_recog_system/AttandanceProject.py
@@ -28,6 +28,68 @@ def findEncodings(iamges):
         encodeList.append(encode)
     return encodeList
 
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0]) #appending name
+        if name not in nameList: #checking if name is in the list
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
 
 encodeListKnown = findEncodings(images)
+print('Encoding Complete')
 #print(len(encodeListKnown))
+
+
+#initializing webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    # img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25) #resizing image into 1/4
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB) #coverting img into rbg
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    #finding matches
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame): #grab one faceloc from facesCurFrame and grab encode of encodeface from facecurrframe
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        #display box around face
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            #print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4 #multiplying values with 4 cuz image was resized into 1/4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
+
+#LABELING UNKNOWN FACES
+if faceDis[matchIndex]< 0.50:
+    name = classNames[matchIndex].upper()
+    markAttendance(name)
+else: name = 'Unknown'
+#print(name)
+y1,x2,y2,x1 = faceLoc
+y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
--- face_recog_system/Basics.py
+++ face_recog_system/Basics.py
@@ -5,7 +5,7 @@ import face_recognition
 #loading image and converting into rbg
 imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
 imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImagesBasic/Mark Z.jpg')
+imgTest = face_recognition.load_image_file('ImagesBasic/Mark Zuckerberg.jpg')
 imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
 #detecting face and their location
